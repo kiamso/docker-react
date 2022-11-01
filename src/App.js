8c97ab0dfe8ba4e9b87188b5602df20e8ad15cda
kiamso <kiamso@gmail.com>
2022-11-01 13:39:04 -0300

    travis.yml added

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HI fiu fiu!! ! !
         </a>
       </header>
     </div>
